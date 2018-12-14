9493eda7f0f8c082db19e4b91fff194b2e7172bd
James Margenberg <james@margenberg.com.au>
2018-12-14 21:42:17 +1100

    Added lift function.

--- lib/monok.ex
+++ lib/monok.ex
@@ -30,4 +30,38 @@ defmodule Monok do
   def fmap({:error, reason}, _function) do
     {:error, reason}
   end
+
+  @doc """
+  Applies a function wrapped in an :ok tuple to a value wrapped in an :ok tuple, carries through an :error
+  tuple if either the value or function arguments are given as :error tuples instead of :ok tuples.
+
+  Examples
+      iex> {:ok, 1}
+      iex> |> Monok.lift({:ok, fn x -> x + 1 end})
+      {:ok, 2}
+
+      iex> {:ok, 1}
+      iex> |> Monok.lift({:error, :reason})
+      {:error, :reason}
+
+      iex> {:ok, 1}
+      iex> |> Monok.lift({:ok, fn x -> x + 1 end})
+      iex> |> Monok.lift({:ok, fn x -> x * 2 end})
+      {:ok, 4}
+
+      iex> {:error, :reason}
+      iex> |> Monok.lift({:ok, fn x -> x + 1 end})
+      {:error, :reason}
+  """
+  def lift({:ok, value}, {:ok, function}) do
+    {:ok, function.(value)}
+  end
+
+  def lift({:error, reason}, _function_tuple) do
+    {:error, reason}
+  end
+
+  def lift(_value_tuple, {:error, reason}) do
+    {:error, reason}
+  end
 end

895be68646374608b79066455609c73c930ac96e
James Margenberg <james@margenberg.com.au>
2018-12-14 21:57:54 +1100

    Added bind function.

--- lib/monok.ex
+++ lib/monok.ex
@@ -64,4 +64,35 @@ defmodule Monok do
   def lift(_value_tuple, {:error, reason}) do
     {:error, reason}
   end
+
+  @doc """
+  Applies a function that returns a value wrapped in an :ok tuple to a value wrapped in an :ok tuple, carries
+  through an :error tuple if either the value argument is given as an :error tuple or the function returns an
+  :error tuple when applied to the value.
+
+  Examples
+      iex> {:ok, 1}
+      iex> |> Monok.bind(fn x -> {:ok, x + 1} end)
+      {:ok, 2}
+
+      iex> {:ok, 1}
+      iex> |> Monok.bind(fn _ -> {:error, :reason} end)
+      {:error, :reason}
+
+      iex> {:ok, 1}
+      iex> |> Monok.bind(fn x -> {:ok, x + 1} end)
+      iex> |> Monok.bind(fn x -> {:ok, x * 2} end)
+      {:ok, 4}
+
+      iex> {:error, :reason}
+      iex> |> Monok.bind(fn x -> {:ok, x + 1} end)
+      {:error, :reason}
+  """
+  def bind({:ok, value}, function) do
+    function.(value)
+  end
+
+  def bind({:error, reason}, _function) do
+    {:error, reason}
+  end
 end

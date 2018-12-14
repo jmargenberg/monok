f46179d525a6b5bdddb8d776ab3d9076c929aff8
James Margenberg <james@margenberg.com.au>
2018-12-14 21:28:35 +1100

    Added fmap function.

--- lib/monok.ex
+++ lib/monok.ex
@@ -1,18 +1,33 @@
 defmodule Monok do
   @moduledoc """
-  Documentation for Monok.
+  Provides simple functor (`|$>`), applicative (`|*>`) and monad (`|=>`) operators for writing pipelines
+  involving functions that return the common `{:ok, result}` or `{:error, reason}` tuples.
   """
 
   @doc """
-  Hello world.
+  Applies a function to a value wrapped in an ok tuple, has no effect if given an error tuple.
 
   ## Examples
 
-      iex> Monok.hello()
-      :world
+      iex> {:ok, 1}
+      iex> |> Monok.fmap(fn x -> x + 1 end)
+      {:ok, 2}
+
+      iex> {:ok, 1}
+      iex> |> Monok.fmap(fn x -> x + 1 end)
+      iex> |> Monok.fmap(fn x -> x * 2 end)
+      {:ok, 4}
+
+      iex> {:error, :reason}
+      iex> |> Monok.fmap(fn x -> x + 1 end)
+      {:error, :reason}
 
   """
-  def hello do
-    :world
+  def fmap({:ok, value}, function) do
+    {:ok, function.(value)}
+  end
+
+  def fmap({:error, reason}, _function) do
+    {:error, reason}
   end
 end

853709f468ed2fcbf217a3d25a8f7f77dbfd660f
James Margenberg <james@margenberg.com.au>
2018-12-16 13:46:46 +1100

    Added test for prefix fmap function.
    Also, moved complex tuple functino into test_helpers.

--- test/monok_test.exs
+++ test/monok_test.exs
@@ -2,17 +2,50 @@ defmodule MonokTest do
   use ExUnit.Case, async: true
   import Monok
 
+  alias MonokTest.Helper
+
   doctest Monok, import: true, except: [:moduledoc]
 
-  setup do
-    complex_tuple_func = fn atom, content ->
-      case atom do
-        :ok -> {:ok, content}
-        other -> {other, content}
-      end
+  describe "fmap" do
+    @tag :standard_functions
+
+    test "chain with :ok tuple literal as input" do
+      assert {:ok, 1}
+             |> fmap(&Integer.to_string/1)
+             |> fmap(&(&1 <> "!")) == {:ok, "1!"},
+             " functions are applied sequentially to value inside of :ok tuple"
     end
 
-    %{complex_tuple_func: complex_tuple_func}
+    test "chain with :error tuple literal as input" do
+      assert {:error, :reason}
+             |> fmap(&Integer.to_string/1)
+             |> fmap(&(&1 <> "!")) == {:error, :reason},
+             ":error tuple is carried through without either function being applied"
+    end
+
+    test "chain with list literal in :ok tuple literal" do
+      assert {:ok, [1, 2, 3]}
+             |> fmap(&Enum.map(&1, fn x -> x + 1 end))
+             |> fmap(&Enum.sum/1)
+             |> fmap(&div(&1, 2)) == {:ok, 4},
+             "both functions are applied to list literal inside :ok tuple"
+    end
+
+    test "chain with map literal in :ok tuple literal" do
+      assert {:ok, %{foo: 1}}
+             |> fmap(&Map.put(&1, :bar, 2))
+             |> fmap(fn map -> Map.update(map, :bar, nil, &(&1 + 2)) end) ==
+               {:ok, %{foo: 1, bar: 4}},
+             "function is applied to map literal inside :ok tuple"
+    end
+
+    @tag :complex_input
+    test "with complex :ok tuple input" do
+      assert Helper.complex_tuple(:ok, 1)
+             |> fmap(&Integer.to_string/1)
+             |> fmap(&(&1 <> "!")) == {:ok, "1!"},
+             "function is applied to value inside of :ok tuple"
+    end
   end
 
   describe "~>" do
@@ -47,10 +80,11 @@ defmodule MonokTest do
              "function is applied to map literal inside :ok tuple"
     end
 
+    test "with complex :ok tuple input"
     # commented out due to infinite compilation bug in ~> macro
     # @tag :complex_input
-    # test "with complex :ok tuple input", %{complex_tuple_func: complex_tuple_func} do
-    #   assert complex_tuple_func.(:ok, 1)
+    # test "with complex :ok tuple input" do
+    #   assert Helper.complex_tuple(:ok, 1)
     #          ~> Integer.to_string()
     #          ~> (&(&1 <> "!")).() == {:ok, "1!"},
     #          "function is applied to value inside of :ok tuple"
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,17 @@
 ExUnit.start()
+
+defmodule MonokTest.Helper do
+  @doc """
+  Returns a tuple that cannot be inferred with Macro.expand. i.e. it must be fully evaluated.
+
+  This is for testing that macros behave correctly when recieving realistic dynamic tuple input rather than
+  static tuple literals that can may be given as a identified in a quoted expression without realy evalutation.
+  """
+  def complex_tuple(atom, content) do
+    case atom do
+      # this
+      :ok -> {:ok, content}
+      other -> {other, content}
+    end
+  end
+end
